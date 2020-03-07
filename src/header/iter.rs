243287af8081513b015aa30f32085760b985006a
The0x539 <the0x539@gmail.com>
2020-03-07 14:59:30 -0600

    tidier header iteration using macros

--- src/header/iter.rs
+++ src/header/iter.rs
@@ -40,68 +40,62 @@ impl TagIter {
         Ok(TagIter {done: false, buf: Cursor::new(buf)})
     }
 
-    fn read_u16(&mut self) -> Result<u16> {
+    fn u16(&mut self) -> Result<u16> {
         self.buf.read_u16::<LE>()
     }
-    fn read_u32(&mut self) -> Result<u32> {
+    fn u32(&mut self) -> Result<u32> {
         self.buf.read_u32::<LE>()
     }
     #[allow(dead_code)]
-    fn read_u64(&mut self) -> Result<u64> {
+    fn u64(&mut self) -> Result<u64> {
         self.buf.read_u64::<LE>()
     }
 
     fn next_tag(&mut self) -> Result<Tag> {
+
+        macro_rules! tag {
+            ($tag:ident: $($memb:ident$(,)?)+) => {
+                Tag::$tag($(self.$memb()?),+)
+            };
+
+            ($tag:ident) => {
+                Tag::$tag
+            };
+        }
+
         if self.done {
             return Err(Error::new(ErrorKind::UnexpectedEof, "No more tags"));
         }
 
-        let tag_type  = self.read_u16()?;
-        let tag_flags = self.read_u16()?;
-        let tag_size  = self.read_u32()?;
+        let tag_type  = self.u16()?;
+        let tag_flags = self.u16()?;
+        let tag_size  = self.u32()?;
 
         let tag = match tag_type {
                 TagType::End  => {
                     self.done = true;
                     Tag::End
                 },
+
                 TagType::InfoRequest => {
                     let mut mbi_tag_types: Vec<u32> = vec![0; (tag_size as usize - 8) / 4];
                     self.buf.read_u32_into::<LE>(&mut mbi_tag_types)?;
                     Tag::InfoRequest(mbi_tag_types)
                 },
-                TagType::LoadAddr => Tag::LoadAddr(
-                    self.read_u32()?,
-                    self.read_u32()?,
-                    self.read_u32()?,
-                    self.read_u32()?,
-                ),
-                TagType::EntryAddr => Tag::EntryAddr(self.read_u32()?),
-                TagType::EntryAddrEfi32 => Tag::EntryAddrEfi32(self.read_u32()?),
-                TagType::EntryAddrEfi64 => Tag::EntryAddrEfi64(self.read_u32()?),
-                TagType::ConsoleFlags => Tag::ConsoleFlags(self.read_u32()?),
-                TagType::Framebuffer => Tag::Framebuffer(
-                    self.read_u32()?,
-                    self.read_u32()?,
-                    self.read_u32()?,
-                ),
-                TagType::ModuleAlign => Tag::ModuleAlign,
-                TagType::EfiBootServices => Tag::EfiBootServices,
-                TagType::Relocatable => Tag::Relocatable(
-                    self.read_u32()?,
-                    self.read_u32()?,
-                    self.read_u32()?,
-                    self.read_u32()?,
-                ),
+
+                TagType::LoadAddr => tag!(LoadAddr: u32, u32, u32, u32),
+                TagType::EntryAddr => tag!(EntryAddr: u32),
+                TagType::EntryAddrEfi32 => tag!(EntryAddrEfi32: u32),
+                TagType::EntryAddrEfi64 => tag!(EntryAddrEfi64: u32),
+                TagType::ConsoleFlags => tag!(ConsoleFlags: u32),
+                TagType::Framebuffer => tag!(Framebuffer: u32, u32, u32),
+                TagType::ModuleAlign => tag!(ModuleAlign),
+                TagType::EfiBootServices => tag!(EfiBootServices),
+                TagType::Relocatable => tag!(Relocatable: u32, u32, u32, u32),
+
                 #[cfg(feature = "hvm")]
-                TagType::HybridRuntime => Tag::HybridRuntime(
-                    self.read_u64()?,
-                    self.read_u64()?,
-                    self.read_u64()?,
-                    self.read_u64()?,
-                    self.read_u64()?,
-                    self.read_u64()?,
-                ),
+                TagType::HybridRuntime => tag!(HybridRuntime: u64, u64, u64, u64, u64, u64),
+
                 _ => Tag::Unknown(tag_type, tag_flags, tag_size),
         };
 
