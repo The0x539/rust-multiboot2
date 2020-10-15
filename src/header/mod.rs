9090b5fafc1a8820f9bdd0daac67d3cde9555cee
The0x539 <the0x539@gmail.com>
2020-10-15 13:55:47 -0500

    header: move iter module's contents out of module

--- src/header/mod.rs
+++ src/header/mod.rs
@@ -1,8 +1,6 @@
 use byteorder::{ReadBytesExt, LE};
 use std::io::{Read, Seek, SeekFrom};
 
-mod iter;
-pub use iter::TagIter;
 mod tag;
 pub use tag::{Tag, TagType};
 
@@ -50,4 +48,35 @@ pub fn find_header<R: Read + Seek>(mut image: R) -> std::io::Result<Option<(u64,
         }
     }
     Ok(None)
+}
+
+#[derive(Debug, Clone)]
+pub struct TagIter<R> {
+    done: bool,
+    data: R,
+}
+
+impl<R: Read> TagIter<R> {
+    pub fn new(data: R) -> Self {
+        Self { done: false, data }
+    }
+}
+
+impl<R: Read> Iterator for TagIter<R> {
+    type Item = std::io::Result<Tag>;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.done {
+            return None;
+        }
+
+        let tag = match Tag::from_reader(&mut self.data) {
+            Ok(t) => t,
+            Err(e) => return Some(Err(e)),
+        };
+        if tag == Tag::End {
+            self.done = true;
+        }
+
+        Some(Ok(tag))
+    }
 }
